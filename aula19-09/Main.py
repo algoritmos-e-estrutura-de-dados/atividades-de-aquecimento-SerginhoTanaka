17119f76bc4a65957ec55e8fe9b60c6a8afe249a
Sergio Kenji Sawasaki Tanaka <serginho.tanaka@edu.unifil.br>
2022-09-23 15:12:27 -0300

    Atualizações

--- aula19-09/Main.py
+++ aula19-09/Main.py
@@ -3,13 +3,19 @@ from lista import Lista
 lista = Lista()
 
 lista.adicionar(1)
-lista.adicionar(2, inicio= True)
+lista.adicionar(2, inicio=True)
 lista.adicionar(3)
-lista.adicionar(4, inicio= True)
+lista.adicionar(4, inicio=True)
 lista.adicionar(5)
-
-
+lista.adicionar(6)
+lista.adicionar(7, inicio=True)
+lista.adicionar(8)
+lista.adicionar(9, inicio=True)
 
 lista.show()
+
 lista.remover(3)
+lista.remover(8)
+lista.remover(6)
+
 lista.show()
--- aula19-09/lista.py
+++ aula19-09/lista.py
@@ -10,7 +10,7 @@ class Lista:
         # adicionar no final da lista
 
     def adicionar(self, valor, inicio=False):
-        if (inicio):
+        if inicio:
             self.adicionar_no_inicio(valor)
         else:
             self.adicionar_no_fim(valor)
@@ -21,33 +21,32 @@ class Lista:
         self.inicio = novo
 
     def adicionar_no_fim(self, valor):
-        if (self.inicio == None):
+        if self.inicio is None:
             self.inicio = Node(valor, None)
         else:
             aux = self.inicio
-            while (aux.proximo != None):
+            while aux.proximo is not None:
                 aux = aux.proximo
 
             aux.proximo = Node(valor, None)
             aux.proximo.anterior = aux
+
     def remover(self, valor):
         aux = self.inicio
         if aux.valor == valor:
-           #aux.valor = None
+            # aux.valor = None
             self.inicio = aux.proximo
         else:
             while aux.proximo is not None:
                 aux = aux.proximo
                 if aux.valor == valor:
                     aux.anterior.proximo = aux.proximo
-                    break;
-
 
     # lista dinamica
     def show(self):
         aux = self.inicio
         print("[", end='')
-        while (aux.proximo != None):
+        while aux.proximo is not None:
             print(f"{aux.valor}", end=', ')
             aux = aux.proximo
         print(f"{aux.valor}]")
